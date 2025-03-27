fe4b479fa735dc50b1c90f2b34316807e227102d
Vikram Seervi <84278571+thevikramseervi@users.noreply.github.com>
2025-03-27 13:12:18 +0000

    26. Odd Divisor

---
+++ 900/26_1475A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void solve() {
+    unsigned long long n;
+    std::cin >> n;
+
+    if (n & (n - 1)) {
+        std::cout << "Yes\n";
+    } else {
+        std::cout << "No\n";
+    }
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("/home/vikram/code/input", "r", stdin);
+    freopen("/home/vikram/code/output", "w", stdout);
+#endif
+
+    int tt = 1;
+    std::cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+    std::cout << '\n';
+    return 0;
+}
