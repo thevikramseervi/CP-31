6e1a76948f32cf39a648fea03dfa058380ba4c49
Vikram Seervi <84278571+thevikramseervi@users.noreply.github.com>
2025-03-31 13:28:15 +0000

    23. Mocha and Math

---
+++ 900/23_1559A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+void solve() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+
+    int ans = a[0];
+    for (int i = 1; i < n; i++) {
+        ans = ans & a[i];
+    }
+
+    std::cout << ans << '\n';
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("/home/vikram/code/input", "r", stdin);
+    freopen("/home/vikram/code/output", "w", stdout);
+#endif
+
+    int tt = 1;
+    std::cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+    std::cout << '\n';
+    return 0;
+}
