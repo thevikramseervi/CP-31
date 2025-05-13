dd3756a880f1b77a0137776ab5ecba13c6b4492c
Vikram Seervi <84278571+thevikramseervi@users.noreply.github.com>
2025-05-13 16:43:15 +0000

    31. Bogo Sort

---
+++ 1000/30_1312B.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+void solve() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+
+    std::sort(a.rbegin(), a.rend());
+
+    // i < j the condition j − aj ≠ i − ai holds
+    for (int i = 0; i < n; i++) {
+        std::cout << a[i] << ' ';
+    }
+    std::cout << '\n';
+}
+
+signed main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("/home/vikram/code/input", "r", stdin);
+    freopen("/home/vikram/code/output", "w", stdout);
+#endif
+
+    int tt = 1;
+    std::cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+    std::cout << '\n';
+    return 0;
+}
