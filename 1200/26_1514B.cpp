3d395b15242c85b79f21f90d9ca697d2b1e199ea
Vikram Seervi <84278571+thevikramseervi@users.noreply.github.com>
2025-08-22 18:30:23 +0530

    26. AND 0, Sum Big

---
+++ 1200/26_1514B.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+const int MOD = 1000000000 + 7;
+
+void solve() {
+    int n, k;
+    std::cin >> n >> k;
+
+    long long ans = n;
+    while (--k) {
+        ans = (ans * n) % MOD;
+    }
+
+    std::cout << ans << '\n';
+}
+
+signed main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("/home/vikram/code/input.txt", "r", stdin);
+    freopen("/home/vikram/code/output.txt", "w", stdout);
+#endif
+
+    int tt = 1;
+    std::cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+
+    return 0;
+}
